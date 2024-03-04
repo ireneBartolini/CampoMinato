689ed970e7afb94c2483c1f2cdc4b969854361a3
ireneBartolini <153780302+ireneBartolini@users.noreply.github.com>
2024-03-04 16:59:26 +0100

    Update Configurazione.js

--- Configurazione.js
+++ Configurazione.js
@@ -1,4 +1,4 @@
-'use strict';
+a'use strict';
 
 class Configurazione extends React.Component {
  
@@ -10,7 +10,7 @@ class Configurazione extends React.Component {
 
             <div className="configuration">
             <h4>Configurazione</h4>
-            <label>inserisci dimensione griglia </label><br></br>
+            <label>inserisci dimensione della griglia </label><br></br>
             <input type="test" id="d" /><br></br>
             <label>inserisci numero di passi</label><br></br>
             <input type="test" id="p" /><br></br>
